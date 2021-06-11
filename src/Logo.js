e065daba138f1210c02ed42a536fa2ede4ce9bd0
Sikhosana Dhliwayo <sikhosana@sikhosanas-mbp.lan>
2021-06-10 20:17:08 -0600

    first commit

---
+++ src/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Logo extends React.Component {
+    render() {
+        return (
+            <>
+                <img alt={"LOGO IMAGE HERE"} width={"50%"} height={"50%"} src={"./public/logo.png"}/>
+            </>
+        );
+    }
+};
+
+export default Logo;
