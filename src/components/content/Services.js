882f35edcfcd47087f0fb5d34cccd697a13a887c
Sikhosana Dhliwayo <sikhosana@sikhosanas-mbp.lan>
2021-06-18 00:50:44 -0600

    dada

--- src/components/content/Services.js
+++ src/components/content/Services.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
+import Carousel from "../Feature Components/Carousel";
+import {CarouselData} from "./CarouselData";
 
 class Services extends Component {
     render() {
         return (
             <>
                 <div>
-                    <h1>WELCOME TO OUR SERVICES PAGE</h1>
+                    <h1>Services</h1> <br/>
+                </div>
+
+                <div>
+                    <Carousel data = {CarouselData} />
                 </div>
             </>
 
---
+++ src/components/content/ServicesData.js
@@ -0,0 +1,40 @@
+import designs from "../../resources/images/designs-01.png"
+import hardscaping from "../../resources/images/hardscaping-01.png"
+import lightscaping from "../../resources/images/lightscaping-01.png"
+import other_services from "../../resources/images/other_services-01.png"
+import softscaping from "../../resources/images/softscaping-01.png"
+import waterscaping from "../../resources/images/waterscaping-01.png"
+
+export const CarouselData =
+    [
+        {
+            image_alt: "image of green",
+            image: designs,
+            caption: "Our Green Project"
+        },
+        {
+            image_alt: "image of red",
+            image: hardscaping,
+            caption: "Our Red Project"
+        },
+        {
+            image_alt: "image of red",
+            image: lightscaping,
+            caption: "Our Red Project"
+        },
+        {
+            image_alt: "image of red",
+            image: other_services,
+            caption: "Our Red Project"
+        },
+        {
+            image_alt: "image of red",
+            image: softscaping,
+            caption: "Our Red Project"
+        },
+        {
+            image_alt: "image of red",
+            image: waterscaping,
+            caption: "Our Red Project"
+        }
+    ]
